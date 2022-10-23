795637e5dd2ad4f2ae3333b58904bdf708a90299
Rafael Jesus <rafaelljesus86@gmail.com>
2022-10-23 10:59:10 +0200

    gitignore

--- hello_world/target/debug/deps/hello_world-49b3767d177af603.d
+++
@@ -1,5 +0,0 @@
-/Users/rafaeljesus/rustbook/hello_world/target/debug/deps/hello_world-49b3767d177af603.rmeta: src/main.rs
-
-/Users/rafaeljesus/rustbook/hello_world/target/debug/deps/hello_world-49b3767d177af603.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/deps/hello_world-ea8a668fae379f44.d
+++
@@ -1,5 +0,0 @@
-/Users/rafaeljesus/rustbook/hello_world/target/debug/deps/hello_world-ea8a668fae379f44: src/main.rs
-
-/Users/rafaeljesus/rustbook/hello_world/target/debug/deps/hello_world-ea8a668fae379f44.d: src/main.rs
-
-src/main.rs:
