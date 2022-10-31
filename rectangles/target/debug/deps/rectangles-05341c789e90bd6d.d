f720b56b219c71cc0cc0bc8236fc783eebbb26f7
Rafael Jesus <rafaelljesus86@gmail.com>
2022-10-31 10:53:20 +0100

    remove target

--- rectangles/target/debug/deps/rectangles-05341c789e90bd6d.d
+++
@@ -1,5 +0,0 @@
-/Users/rafaeljesus/rustbook/rectangles/target/debug/deps/rectangles-05341c789e90bd6d: src/main.rs
-
-/Users/rafaeljesus/rustbook/rectangles/target/debug/deps/rectangles-05341c789e90bd6d.d: src/main.rs
-
-src/main.rs:
--- rectangles/target/debug/rectangles.d
+++
@@ -1,1 +0,0 @@
-/Users/rafaeljesus/rustbook/rectangles/target/debug/rectangles: /Users/rafaeljesus/rustbook/rectangles/src/main.rs
