2980c60c0b4e8abef4bf3f385a7037dbb9116401
Rafael Jesus <rafaelljesus86@gmail.com>
2022-12-04 11:47:55 +0100

    improve tests

--- binary_tree/src/lib.rs
+++ binary_tree/src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq)]
+#[derive(PartialEq)]
 pub struct Node {
     value: i32,
     left: Option<Box<Node>>,
@@ -22,21 +22,52 @@ mod tests {
 
     #[test]
     fn invert_binary_tree() {
+        //        1
+        //       / \
+        //      2   3
+        //     / \ / \
+        //    4  5 6  7
         let mut tree = Some(Box::new(Node {
             value: 1,
             left: Some(Box::new(Node {
                 value: 2,
-                left: None,
-                right: None,
+                left: Some(Box::new(Node {
+                    value: 4,
+                    left: None,
+                    right: None,
+                })),
+                right: Some(Box::new(Node {
+                    value: 5,
+                    left: None,
+                    right: None,
+                })),
             })),
             right: Some(Box::new(Node {
-                value: 5,
-                left: None,
-                right: None,
+                value: 3,
+                left: Some(Box::new(Node {
+                    value: 6,
+                    left: None,
+                    right: None,
+                })),
+                right: Some(Box::new(Node {
+                    value: 7,
+                    left: None,
+                    right: None,
+                })),
             })),
         }));
         invert_tree(&mut tree);
-        assert_eq!(tree.as_ref().unwrap().left.as_ref().unwrap().value, 5);
+        //        1
+        //       / \
+        //      3   2
+        //     / \ / \
+        //    7  6 5  4
+        assert_eq!(tree.as_ref().unwrap().left.as_ref().unwrap().value, 3);
+        assert_eq!(tree.as_ref().unwrap().left.as_ref().unwrap().left.as_ref().unwrap().value, 7);
+        assert_eq!(tree.as_ref().unwrap().left.as_ref().unwrap().right.as_ref().unwrap().value, 6);
+
         assert_eq!(tree.as_ref().unwrap().right.as_ref().unwrap().value, 2);
+        assert_eq!(tree.as_ref().unwrap().right.as_ref().unwrap().left.as_ref().unwrap().value, 5);
+        assert_eq!(tree.as_ref().unwrap().right.as_ref().unwrap().right.as_ref().unwrap().value, 4);
     }
 }
